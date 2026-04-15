4592f7e1c8be0d2bd00822ebfddd8678b4030598
Gabriel Martins <gabrielmonte.aluno.institute@gmail.com>
2026-04-15 12:09:26 -0300

    Adicionando Desing System

---
+++ Reminder/Sources/Colors/Colors-Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Gabriel Monte Olivio Martins on 15/04/26.
+//
+
+import Foundation
+
+/// Iniciando o Design System para padronizar e não depender de RGB/HEX diretamente na View e ser possivel reutilização.
+
